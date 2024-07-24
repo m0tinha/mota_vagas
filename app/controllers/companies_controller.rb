3c571a863d945bbcb63307e86b91d9ad736a620b
Marcelo Mota <mmarcelomota123@gmail.com>
2024-07-24 09:06:19 -0400

    ajustando a aba minha empresa

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,41 @@
 class CompaniesController < ApplicationController
   def new
-  end
-
-  def edit
+    if current_user.company
+      redirect_to edit_company_path(current_user.company), alert: "Você já possui uma empresa. Edite sua empresa existente."
+    else
+      @company = current_user.build_company
+    end
   end
 
   def create
+    @company = current_user.build_company(company_params)
+    if @company.save
+      flash[:notice] = 'Sua Empresa foi cadastrada com sucesso!!!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Erro ao cadastrar a empresa.'
+      render :new
+    end  
   end
-  
-  def ::update,
+
+  def edit
+    @company = current_user.company
+  end
+
+  def update
+    @company = current_user.company
+    if @company.update(company_params)
+      flash[:notice] = 'Empresa atualizada com sucesso.'
+      redirect_to root_path
+    else
+      flash[:error] = 'Erro ao atualizar a empresa.'
+      render :edit
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :url)
   end
 end
